9b0a87d0fb577583b236fa511c9f9197f40f5631
Benjamin Strumeyer <benjamin.strumeyer@stonybrook.edu>
2016-04-15 22:20:33 -0400

    We can initialize the diagonal now.

--- src/core/parsers/CKYParser.ts
+++ src/core/parsers/CKYParser.ts
@@ -18,23 +18,33 @@ export class CKYParser {
 
         //This is the diagonal initializing loop
         for (let i = 0; i < words.length; i++) {
+
             // Find all the rules that have the right hand side as the word i.
             let word = words[i];
+
             // Find all the rules in the grammar with word on the right side.
+            let tuples = this.findLHSForNonTerminal(word);
 
-
-            this.table[i][i] = [];
-
+            this.table[i][i] = tuples;
         }
+
+
     }
 
     private findLHSForNonTerminal(word:string):NonTerminalTuple[] {
         var rules = this.grammar.rules;
 
         var matchingLefts = rules
-            .filter(rule => rule.isUnary())
-            .filter(rule => rule.right[0] === word);
+            .filter(rule => rule.isUnary() && rule.right[0] === word);
 
+        var tuples = matchingLefts
+            .map(x =>
+            {
+                let t = new NonTerminalTuple();
+                t.nonTerminal = x;
+                return t;
+            });
 
+        return tuples;
     }
 }
