4c5efc335d2d512cb2f538c9d533cead755ebbc5
cypherix93 <bikram.ghosh93@gmail.com>
2016-04-16 23:00:22 -0400

    Updated the handler to output the parsed.trees file after the CKY parser parses the stuff.

--- src/app/handlers/CKYHandler.ts
+++ src/app/handlers/CKYHandler.ts
@@ -2,6 +2,7 @@ import {FileWorker} from "../../core/FileWorker";
 import {Preprocess} from "../../core/Preprocess";
 import {CKYParser} from "../../core/parsers/CKYParser";
 import {_HandlerBase} from "./_HandlerBase";
+import {TreeParser} from "../../core/parsers/TreeParser";
 
 export class CKYHandler
 {
@@ -18,12 +19,11 @@ export class CKYHandler
         for (let line of lines)
         {
             var parsedTree = this.parser.parse(line);
-            parsedTrees.push(parsedTree);
+            var parsedTreeString = TreeParser.deparseTree(parsedTree);
+            parsedTrees.push(parsedTreeString);
         }
 
-        // Do something here with the parsed trees
-        // I guess we have to deparse the parsedTrees into string format like in test.trees
-        // And then compare them? We should just output a file like parsed.trees
-        // Then run his Python script or whatever
+        var parsedText = parsedTrees.join("\n");
+        FileWorker.writeTextFile("./output/parsed.trees", parsedText);
     }
 }
