037b0dac54e480db5cbf22eebd5e7e1c1288bdbd
cypherix93 <bikram.ghosh93@gmail.com>
2016-04-18 21:24:11 -0400

    Added progress bars :DDD

--- dist/core/grammar/_GrammarBase.js
+++ dist/core/grammar/_GrammarBase.js
@@ -1,5 +1,6 @@
 "use strict";
 const _ = require("lodash");
+const ProgressBar = require("progress");
 const GrammarRule_1 = require("../models/GrammarRule");
 const TreeParser_1 = require("../parsers/TreeParser");
 const Preprocess_1 = require("../Preprocess");
@@ -7,11 +8,17 @@ const LaplaceSmoothingMethod_1 = require("../methods/LaplaceSmoothingMethod");
 class _GrammarBase {
     train(unparsedTrees) {
         var treeLines = Preprocess_1.Preprocess.getLines(unparsedTrees);
+        var progressBar = new ProgressBar("Building grammar from test.trees for :elapseds [:bar] :percent", {
+            total: treeLines.length,
+            width: 50,
+            complete: "#"
+        });
         var allRules = [];
         for (let tree of treeLines) {
             let parsedTree = TreeParser_1.TreeParser.parseTree(tree);
             let rules = this.convertTreeToRules(parsedTree);
             allRules = allRules.concat(rules);
+            progressBar.tick();
         }
         var uniqueRules = _.uniqWith(allRules, (x, y) => {
             var isEqual = x.equals(y);
@@ -27,8 +34,14 @@ class _GrammarBase {
     computeProbabilitiesOfRules() {
         var rules = this.rules;
         var laplaceMethod = new LaplaceSmoothingMethod_1.LaplaceSmoothingMethod(rules);
+        var progressBar = new ProgressBar("Computing probabilities for rules for :elapseds [:bar] :percent", {
+            total: rules.length,
+            width: 50,
+            complete: "#"
+        });
         for (let rule of rules) {
             rule.probability = laplaceMethod.computeProbability(rule);
+            progressBar.tick();
         }
     }
     convertTreeToRules(tree) {
